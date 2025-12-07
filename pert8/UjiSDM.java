656d3dafe43c56901003b4c7f4b860b5c9e5e135
yazid-nugraha <yazid.nugraha@mhs.unsoed.ac.id>
2025-12-07 23:17:34 +0700

    lop yu asrpak

--- pert8/UjiSDM.java
+++ pert8/UjiSDM.java
@@ -1,28 +1,13 @@
 public class UjiSDM {
     public static void main(String[] args) {
-        
-        // ==========================================================
-        // DATA UJI COBA
-        // ==========================================================
-        
-        // 1. BUAT OBJEK ProgrammerMagang
-        // Gunakan constructor dengan data: "Andi", 50000.0, "1234".
-        
-        
-        // 2. PENGUJIAN METODE BERURUTAN
-        
-        // Panggil method hitungGaji(160).
-        
-        // Cetak hasil dari method getStatusCuti().
-        
-        // Panggil login("9999") (Tes PIN salah).
-        
-        // Panggil login("1234") (Tes PIN benar).
-        
-        // Cetak hasil dari method getRoleAkses().
-        
-        // Panggil perpanjangKontrak(6).
-        
-        // Panggil logout().
+        System.out.println("=== PENGUJIAN PROGRAMMER MAGANG ===");
+        ProgrammerMagang andi = new ProgrammerMagang("Andi", 50000.0, "1234");
+        andi.hitungGaji(160);
+        System.out.println("Status Cuti: " + andi.getStatusCuti());
+        andi.login("9999");
+        andi.login("1234");
+        System.out.println("Role Akses: " + andi.getRoleAkses());
+        andi.perpanjangKontrak(6);
+        andi.logout();
     }
 }
